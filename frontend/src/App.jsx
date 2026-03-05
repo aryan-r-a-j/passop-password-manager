0f571d3fc8e70762df257711ac5e021973e01e8d
Aryan Raj <aryansncs@gmail.com>
2026-03-05 12:40:04 +0530

    Fix case-sensitive component imports for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 function App() {
